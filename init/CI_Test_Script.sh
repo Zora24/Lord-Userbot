2c48cd1666e4cdf3ec128c94ec339301b3111b96
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-11 01:42:39 +0700

    Update CI_Test_Script.sh

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -13,7 +13,7 @@ export SEMAPHORE_PROJECT_DIR=`pwd`
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
-tg_sendinfo "<code>I am gonna merge staging into sql-extended</code>"
+tg_sendinfo "<code>I am gonna merge staging into Lord-Userbot</code>"
 cd
 git clone https://github.com/mkaraniya/OpenUserBot.git
 cd OpenUserBot
