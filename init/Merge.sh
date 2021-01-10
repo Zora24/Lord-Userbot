6e1d6a01a36886b7a196dec8d8051941dfe51735
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-11 01:41:28 +0700

    Update Merge.sh

--- init/Merge.sh
+++ init/Merge.sh
@@ -13,7 +13,7 @@ export SEMAPHORE_PROJECT_DIR=`pwd`
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
-tg_sendinfo "<code>I am gonna merge staging into sql-extended</code>"
+tg_sendinfo "<code>I am gonna merge staging into Lord-Userbot</code>"
 cd
 git clone https://github.com/sahyam2019/oub-remix.git
 cd oub-remix
@@ -22,4 +22,4 @@ git fetch
 git checkout staging
 git pull origin staging
 git push --force origin staging:sql-extended
-tg_sendinfo "<code>I have merged all commits from staging into sql-extended</code>"
+tg_sendinfo "<code>I have merged all commits from staging into Lord-Userbot</code>"
