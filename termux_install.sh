14f9646ab70e2034a825148e7d604af43fc91d41
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-11 23:18:56 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -34,8 +34,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/mkaraniya/OpenUserBot.git
-cd OpenUserBot
+git clone https://github.com/Zora24/Lord-Userbot.git
+cd Lord-Userbot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
