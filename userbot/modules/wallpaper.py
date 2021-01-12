ffb495c1fcf5c143a4f3b41fb064ba9f7ba92f52
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-12 21:53:03 +0700

    Update wallpaper.py

--- userbot/modules/wallpaper.py
+++ userbot/modules/wallpaper.py
@@ -18,8 +18,8 @@ from userbot.events import register
 async def _(event):
     try:
         query = event.pattern_match.group(1)
-        await event.edit("`Processing..`")
-        async with bot.conversation("@XBOTGBOT") as conv:
+        await event.edit("`Mohon Menunggu Lord Saya Sedang Mencari Wallpaper.....`")
+        async with bot.conversation("@SaitamaRobot") as conv:
             try:
                 query1 = await conv.send_message(f"/wall {query}")
                 asyncio.sleep(3)
@@ -27,18 +27,18 @@ async def _(event):
                 r2 = await conv.get_response()
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
-                return await event.reply("Unblock @XBOTGBOT plox")
+                return await event.reply("`Maaf Tidak Bisa`")
             if r1.text.startswith("No"):
-                return await event.edit(f"`No result found for` **{query}**")
+                return await event.edit(f"`Saya Tidak Menemukan` **{query}**")
             else:
                 img = await event.client.download_media(r1)
                 img2 = await event.client.download_media(r2)
-                await event.edit("`Uploading..`")
+                await event.edit("`Sedang Mengunggah..`")
                 p = await event.client.send_file(
                     event.chat_id,
                     img,
                     force_document=False,
-                    caption="Preview",
+                    caption="Ini Wallpaer Yang Anda Cari Lord",
                     reply_to=event.reply_to_msg_id,
                 )
                 await event.client.send_file(
@@ -54,8 +54,8 @@ async def _(event):
         await event.delete()
         os.system("rm *.png *.jpg")
     except TimeoutError:
-        return await event.edit("`@XBOTGBOT isnt responding..`")
+        return await event.edit("`Saya Sedang Lelah....`")
 
 
 CMD_HELP.update({"wallpaper": ">`.wall <query>`"
-                 "\nUsage: search beautiful wallpaper image."})
+                 "\nUsage: Mencari Wallpaper Bagus."})
