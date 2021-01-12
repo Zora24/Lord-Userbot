5344a6091fcf609011f4492180c2c3ddd707ee21
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-12 22:26:08 +0700

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -21,7 +21,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "__Membongkar Kedok User...__")
+        "`Mengambil Informasi Pengguna Ini...`")
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
@@ -31,7 +31,7 @@ async def who(event):
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        return event.edit("`Could not fetch info of that user.`")
+        return event.edit("`Saya Tidak Mendapatkan Informasi Apapun.`")
 
     message_id_to_reply = event.message.reply_to_msg_id
 
@@ -96,7 +96,7 @@ async def fetch_info(replied_user, event):
                              offset=42,
                              max_id=0,
                              limit=80))
-    replied_user_profile_photos_count = "Person needs help with uploading profile picture."
+    replied_user_profile_photos_count = "Orang tersebut membutuhkan bantuan untuk mengupload gambar profil."
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
     except AttributeError:
@@ -107,7 +107,7 @@ async def fetch_info(replied_user, event):
     try:
         dc_id, location = get_input_location(replied_user.profile_photo)
     except Exception as e:
-        dc_id = "Couldn't fetch DC ID!"
+        dc_id = "Tidak Dapat Mengambil DC ID!"
         str(e)
     common_chat = replied_user.common_chats_count
     username = replied_user.user.username
@@ -127,19 +127,19 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    caption = "<b>USER INFO:</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
+    caption = "<b>**INFORMASI PENGGUNA**:</b>\n\n"
+    caption += f"Nama Pertama: {first_name}\n"
+    caption += f"Nama Terakhir: {last_name}\n"
     caption += f"Username: {username}\n"
     caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
+    caption += f"Total Foto Profil: {replied_user_profile_photos_count}\n"
+    caption += f"Apakah Bot: {is_bot}\n"
+    caption += f"Dibatasi: {restricted}\n"
+    caption += f"Diverifikasi Oleh Telegram: {verified}\n"
     caption += f"ID: <code>{user_id}</code>\n\n"
     caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Permanent Link To Profile: "
+    caption += f"Obrolan Umum Dengan Pengguna Ini: {common_chat}\n"
+    caption += f"Link Permanen Ke Profil: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
@@ -147,6 +147,6 @@ async def fetch_info(replied_user, event):
 
 CMD_HELP.update({
     "whois":
-    ">`.whois <username> or reply to someones text with .whois`"
-    "\nUsage: Gets info of an user."
+    ">`.whois <username> Atau Balas Ke Pesan Pengguna Ketik .whois`"
+    "\nUsage: Mendapatkan Informasi Pengguna."
 })
