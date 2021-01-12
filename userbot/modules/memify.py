8b47adb795b3964a4452edd2594bfa3ac0ebd93c
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-12 19:43:49 +0700

    Update memify.py

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -214,7 +214,7 @@ async def draw_meme_text(image_path, text):
     os.remove(image_path)
     i_width, i_height = img.size
     m_font = ImageFont.truetype(
-        "userbot/utils/styles/ProductSans-BoldItalic.ttf", int((95 / 730) * i_width)
+        "userbot/utils/styles/FontLord.ttf", int((95 / 730) * i_width)
     )
     if ";" in text:
         upper_text, lower_text = text.split(";")
