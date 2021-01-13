bccd071332940d1a9f91a20ffedee33bde005cc5
Alvin <75377410+Zora24@users.noreply.github.com>
2021-01-13 20:58:02 +0700

    Update lord.py

--- userbot/modules/lord.py
+++ userbot/modules/lord.py
@@ -6,9 +6,9 @@ from userbot.events import register
 @register(outgoing=True, pattern='^.sadboy(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(2)
     await typew.edit("`Pertama-tama kamu cantik`")
-    sleep(3)
+    sleep(2)
     await typew.edit("`Kedua kamu manis`")
     sleep(1)
     await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
@@ -18,9 +18,9 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.lord(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
-    await typew.edit("`HALLO LORD AKU ADALAH BOT, DAN AKU AKAN SELALU MEMBANTU MU`")
     sleep(1)
+    await typew.edit("`HALLO LORD AKU ADALAH BOT, DAN AKU AKAN SELALU MEMBANTU MU UNTUK BERSENANG-SENANG DI DUNIA TELEGRAM`")
+    sleep(2)
     await typew.edit("`\n█████████`"
                      "`\n█▄█████▄█`"
                      "`\n█▼▼▼▼▼`"
@@ -49,6 +49,14 @@ async def typewriter(typew):
     sleep(1)
     await typew.edit("` \n   ╲╲╭━━━━╮ \n╭╮┃▆┈┈▆┃╭╮ \n┃╰┫★★★┣╯┃ \n╰━┫★★★┣━╯`"
                      "`\n╲╲┃┈┈┈┈┃  \n╲╲┃┈┏┓┈┃ `")
+    sleep(3)
+    await typew.edit("`\n█████████`"
+                     "`\n█▄█████▄█`"
+                     "`\n█▼▼▼▼▼`"
+                     "`\n█    AKU ADALAH LORD`"
+                     "`\n█▲▲▲▲▲`"
+                     "`\n█████████`"
+                     "`\n ██   ██`")
 
 
 # Create by myself @localheart
