e5089047b870af9dfc9a9060634be838d73cfc6b
Ependelope53 <78998317+Ependelope53@users.noreply.github.com>
2021-02-20 14:33:56 +0700

    Update memify.py

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -30,7 +30,7 @@ async def mim(event):
         return
     else:
         await event.edit(
-            "```Mengubah Gambar Ini Mwahaha Saatnya Menulis ツ ```"
+            "```Mengubah Gambar Ini XiXiXi Saatnya Mencetak ツ ```"
         )
         await asyncio.sleep(5)
         text = event.pattern_match.group(1)
@@ -186,7 +186,7 @@ async def mim(event):
         return
     else:
         await event.edit(
-            "```Mengubah Gambar Ini Mwahaha Saatnya Menulis ツ ```"
+            "```Mengubah Gambar Ini XiXiXi Saatnya Mencetak ツ ```"
         )
         await asyncio.sleep(5)
         text = event.pattern_match.group(1)
