57375c6133a9114f18315623495208fad0c4018b
Ependelope53 <78998317+Ependelope53@users.noreply.github.com>
2021-02-22 01:50:23 +0700

    Update emojigames.py

--- userbot/modules/emojigames.py
+++ userbot/modules/emojigames.py
@@ -54,7 +54,55 @@ async def _(event):
                 await r.delete()
                 r = await event.reply(file=InputMediaDice('🏀'))
         except BaseException:
-            pass
+            passpass
+
+
+@bot.on(admin_cmd(pattern="ustat ?(.*)"))
+@bot.on(sudo_cmd(pattern="ustat ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    reply_message = await event.get_reply_message()
+    if not input_str and not reply_message:
+        await edit_delete(
+            event,
+            "`reply to  user's text message to get name/username history or give userid/username`",
+        )
+    if input_str:
+        try:
+            uid = int(input_str)
+        except ValueError:
+            try:
+                u = await event.client.get_entity(input_str)
+            except ValueError:
+                await edit_delete(
+                    event, "`Give userid or username to find name history`"
+                )
+            uid = u.id
+    else:
+        uid = reply_message.sender_id
+    chat = "@tgscanrobot"
+    catevent = await edit_or_reply(event, "`Processing...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message(f"{uid}")
+        except Exception:
+            await edit_delete(catevent, "`unblock `@tgscanrobot` and then try`")
+        response = await conv.get_response()
+        await event.client.send_read_acknowledge(conv.chat_id)
+        await catevent.edit(response.text)
+
+
+def inline_mention(user):
+    full_name = user_full_name(user) or "No Name"
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+def user_full_name(user):
+    names = [user.first_name, user.last_name]
+    names = [i for i in list(names) if i]
+    return " ".join(names)
 
 CMD_HELP.update({
     "emojigames":
