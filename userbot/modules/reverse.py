2f339882ba9dd4ac9fc85d88224a936850b4a542
Ependelope53 <78998317+Ependelope53@users.noreply.github.com>
2021-02-22 00:26:28 +0700

    Update reverse.py

--- userbot/modules/reverse.py
+++ userbot/modules/reverse.py
@@ -35,7 +35,7 @@ async def okgoogle(img):
         photo = io.BytesIO()
         await bot.download_media(message, photo)
     else:
-        await img.edit("`Lord Mohon Balas Ke Sticker`")
+        await img.edit("`Mohon Balas Ke Sticker Sayang`")
         return
 
     if photo:
@@ -80,7 +80,7 @@ async def okgoogle(img):
         if guess and imgspage:
             await img.edit(f"[{guess}]({fetchUrl})\n\n`Sedang Mencari Gambar Yang Mirip...`")
         else:
-            await img.edit("`Maaf Lord, Saya Tidak Bisa Menemukan Apapun`")
+            await img.edit("`Maaf Sayang, Saya Tidak Bisa Menemukan Apapun`")
             return
 
         if img.pattern_match.group(1):
