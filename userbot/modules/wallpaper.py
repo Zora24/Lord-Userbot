168952518641be5b60d93cfdc99fdb5a79ebf383
Ependelope53 <78998317+Ependelope53@users.noreply.github.com>
2021-02-22 19:01:04 +0700

    Update wallpaper.py

--- userbot/modules/wallpaper.py
+++ userbot/modules/wallpaper.py
@@ -18,7 +18,7 @@ from userbot.events import register
 async def _(event):
     try:
         query = event.pattern_match.group(1)
-        await event.edit("`Mohon Menunggu Lord Saya Sedang Mencari Wallpaper.....`")
+        await event.edit("`Mohon Menunggu Sayang Saya Sedang Mencari Wallpaper.....`")
         async with bot.conversation("@SaitamaRobot") as conv:
             try:
                 query1 = await conv.send_message(f"/wall {query}")
