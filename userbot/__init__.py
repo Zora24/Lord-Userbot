5e84ea268c0bc80f996ac692276d9079f01c6a06
Hendra Putra Tanjung <78998317+Ependelope53@users.noreply.github.com>
2021-02-24 09:49:26 +0700

    Revert "Update __init__.py"

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -399,7 +399,7 @@ with bot:
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.article(
                     "Harap Gunakan .help Untuk Perintah",
-                    text="{}\n\n**❃ Jumlah Modul Yang Tersedia:** `{}`\n               \n**❃ Daftar Modul GabutC-UBot:** \n".format(
+                    text="{}\n\n**❃ Jumlah Modul Yang Tersedia:** `{}`\n               \n**❃ Daftar Modul Lord Userbot:** \n".format(
                         "**❂ GabutC-UBot**",
                         len(dugmeler),
                     ),
