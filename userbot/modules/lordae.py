e3aed23b61a640bf49dbe5b870b2b46a49513958
Alvin <75377410+Zora24@users.noreply.github.com>
2021-02-27 01:09:46 +0700

    Create lordae.py

---
+++ userbot/modules/lordae.py
@@ -0,0 +1,35 @@
+# Based Plugins
+# Ported for Lord-Userbot By liualvinas/Alvin
+
+from telethon import events
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+PRINTABLE_ASCII = range(0x21, 0x7F)
+
+
+def aesthetify(string):
+    for c in string:
+        c = ord(c)
+        if c in PRINTABLE_ASCII:
+            c += 0xFF00 - 0x20
+        elif c == ord(" "):
+            c = 0x3000
+        yield chr(c)
+
+
+@register(outgoing=True, pattern=r"^\.ae(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    text = event.pattern_match.group(1)
+    text = "".join(aesthetify(text))
+    await event.edit(text=text, parse_mode=None, link_preview=False)
+    raise events.StopPropagation
+
+
+CMD_HELP.update({
+    "aeshtetic":
+    "`.ae <teks>`\
+    \nUsage: mengubah font teks"
+})
