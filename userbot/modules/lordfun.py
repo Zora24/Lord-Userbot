344b6df918b327efa953e65d5eae35a329fce002
Alvin <75377410+Zora24@users.noreply.github.com>
2021-02-27 02:09:43 +0700

    Create lordfun.py

---
+++ userbot/modules/lordfun.py
@@ -0,0 +1,57 @@
+# Based Plugins
+# Ported For Lord-Userbot By liualvinas/Alvin
+# If You Kang It Don't Delete / Warning!! Jangan Hapus Ini!!!
+
+from lordvars import Config
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.xogame(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+# Alvin Gans 
+@register(outgoing=True, pattern=r"^\.wp(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    wwwspr = event.pattern_match.group(1)
+    botusername = "@whisperBot"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, wwwspr)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+# Alvin Gans
+@register(outgoing=True, pattern=r"^\.mod(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@PremiumAppBot"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+# Ported For Lord-Userbot By liualvinas/Alvin
+
+CMD_HELP.update({
+    "lordfun": "\
+`.xogame`\
+\nUsage: Mainkan game XO bersama temanmu.\
+\n\n`.mod <nama app>`\
+\nUsage: Dapatkan applikasi mod\
+\n\n`.wp <teks> <username/ID>`\
+\nUsage: Berikan pesan rahasia"})
