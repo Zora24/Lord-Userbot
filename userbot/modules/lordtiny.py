3ebdfd975c7febdeb0d55f254f58e570dda3f8f4
Alvin <75377410+Zora24@users.noreply.github.com>
2021-03-05 01:56:04 +0700

    Create lordtiny.py

---
+++ userbot/modules/lordtiny.py
@@ -0,0 +1,87 @@
+# Based From Plugins
+# Ported For Lord-Userbot By liualvinas/Alvin
+
+import os
+from userbot.events import register
+from telethon import events
+from userbot import CMD_HELP
+from PIL import Image
+
+
+@register(outgoing=True, pattern="^.tiny(?: |$)(.*)", disable_errors=True)
+async def ultiny(event):
+    reply = await event.get_reply_message()
+    if not (reply and (reply.media)):
+        await eor(event, "`Reply To Media`")
+        return
+    xx = await eor(event, "`processing...`")
+    ik = await userbot.download_media(reply)
+    im1 = Image.open("resources/extras/ultroid_blank.png")
+    if ik.endswith(".tgs"):
+        await event.client.download_media(reply, "ult.tgs")
+        os.system("lottie_convert.py ult.tgs json.json")
+        json = open("json.json", "r")
+        jsn = json.read()
+        json.close()
+        jsn = jsn.replace("512", "2000")
+        open("json.json", "w").write(jsn)
+        os.system("lottie_convert.py json.json ult.tgs")
+        file = "ult.tgs"
+        os.remove("json.json")
+    elif ik.endswith((".gif", ".mp4")):
+        iik = cv2.VideoCapture(ik)
+        dani, busy = iik.read()
+        cv2.imwrite("i.png", busy)
+        fil = "i.png"
+        im = Image.open(fil)
+        z, d = im.size
+        if z == d:
+            xxx, yyy = 200, 200
+        else:
+            t = z + d
+            a = z / t
+            b = d / t
+            aa = (a * 100) - 50
+            bb = (b * 100) - 50
+            xxx = 200 + 5 * aa
+            yyy = 200 + 5 * bb
+        k = im.resize((int(xxx), int(yyy)))
+        k.save("k.png", format="PNG", optimize=True)
+        im2 = Image.open("k.png")
+        back_im = im1.copy()
+        back_im.paste(im2, (150, 0))
+        back_im.save("o.webp", "WEBP", quality=95)
+        file = "o.webp"
+        os.remove(fil)
+        os.remove("k.png")
+    else:
+        im = Image.open(ik)
+        z, d = im.size
+        if z == d:
+            xxx, yyy = 200, 200
+        else:
+            t = z + d
+            a = z / t
+            b = d / t
+            aa = (a * 100) - 50
+            bb = (b * 100) - 50
+            xxx = 200 + 5 * aa
+            yyy = 200 + 5 * bb
+        k = im.resize((int(xxx), int(yyy)))
+        k.save("k.png", format="PNG", optimize=True)
+        im2 = Image.open("k.png")
+        back_im = im1.copy()
+        back_im.paste(im2, (150, 0))
+        back_im.save("o.webp", "WEBP", quality=95)
+        file = "o.webp"
+        os.remove("k.png")
+    await event.client.send_file(event.chat_id, file, reply_to=event.reply_to_msg_id)
+    await xx.delete()
+    os.remove(file)
+    os.remove(ik)
+
+CMD_HELP.update(
+    {
+        "tiny": "`.tiny`\
+    \nPenjelasan: Merubah Sticker Menjadi Kecil."
+    })
