73b817c2ac1995a80ae32e0853908809c23f2193
KENZO-420 <78176892+KENZO-420@users.noreply.github.com>
2021-03-09 05:18:32 +0000

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -16,7 +16,7 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamu'alaikum")
+    await typew.edit("`Assalamu'alaikum...`🙏")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
@@ -28,7 +28,7 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumussalam")
+    await typew.edit("`Wa'alaikumussalam xixixi`😁")
 
 
 CMD_HELP.update({
