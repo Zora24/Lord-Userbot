5439e1f802f0001f285033477f1edf8d123252e2
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-03-12 02:50:39 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -423,11 +423,11 @@ with bot:
                     buttons=[
                         [
                             custom.Button.url(
-                                "Repo Lord-Userbot",
+                                "Repo STRESS-Userbot",
                                 "https://github.com/ramadhani892/STRESS-Userbot"),
                             custom.Button.url(
                                 "Pemilik Repo",
-                                "t.me/liualvinas")],
+                                "t.me/Ramadhaniiiiiiiii1")],
                     ],
                     link_preview=False,
                 )
@@ -447,7 +447,7 @@ with bot:
                 # https://t.me/TelethonChat/115200
                 await event.edit(buttons=buttons)
             else:
-                reply_pop_up_alert = f"Harap Deploy Lord Userbot Anda Sendiri, Jangan Menggunakan Milik Lord {ALIVE_NAME} ツ"
+                reply_pop_up_alert = f"Harap Deploy Stress Userbot Anda Sendiri, Jangan Menggunakan Milik Lord {ALIVE_NAME} ツ"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @tgbot.on(
