8e09ffb23f50c4ae374c30384d7aebd09b8f4bff
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-03-13 20:13:01 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -103,7 +103,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/ramadhani892/STRESS-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Lord-Userbot")
+    "UPSTREAM_REPO_BRANCH", "STRESS-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
