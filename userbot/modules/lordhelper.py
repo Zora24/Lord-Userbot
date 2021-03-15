c8f6f5935a976b4bef1b151ad1f30141ec98969b
Frisky395 <79519962+Frisky395@users.noreply.github.com>
2021-03-15 15:23:43 +0700

    Update lordhelper.py

--- userbot/modules/lordhelper.py
+++ userbot/modules/lordhelper.py
@@ -8,16 +8,16 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.lordhelp$")
+@register(outgoing=True, pattern="^.lhelp$")
 async def usit(e):
     await e.edit(
-        f"**Hai Lord {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        f"**Hai Lord {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
         "\n[Telegram](t.me/liualvinas)"
         "\n[Repo](https://github.com/zora24/Lord-Userbot)"
         "\n[Instagram](Instagram.com/liualvinas_)")
 
 
-@register(outgoing=True, pattern="^.lordvar$")
+@register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
@@ -27,7 +27,7 @@ async def var(m):
 CMD_HELP.update({
     "lordhelper":
     "`.lordhelp`\
-\nUsage: Bantuan Untuk Lord-Userbot.\
+\nPenjelasan: Bantuan Untuk Lord-Userbot.\
 \n`.lordvar`\
-\nUsage: Melihat Daftar Vars."
+\nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
