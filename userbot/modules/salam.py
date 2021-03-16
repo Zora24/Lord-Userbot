5772a50253b72bf9a9e50081e0c5c78403e60b70
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-03-16 22:05:31 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,19 +10,19 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamu'alaikum Kalian")
+    await typew.edit("Assalamu'alaikum ")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamu'alaikum Semua")
+    await typew.edit("Assalamu'alaikum, Gimana Kabar Kalian?")
 
 
 @register(outgoing=True, pattern='^L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumsalam cuyy ")
+    await typew.edit("Wa'alaikumsalam")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
