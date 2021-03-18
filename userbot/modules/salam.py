360e25b1bec70ad78177b6d449f56b3815955023
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-03-19 05:14:58 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -76,12 +76,14 @@ async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**MACAM BAGUS AE LU BEGITU KONTOL!!**")
 
+
 @register(outgoing=True, pattern='^J(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**JELEK BANGET LU, NAJIS CUIHHHH!!!**")
 
-@register(outgoinnh=True, pattern='^A(?: |$)(.*)')
+
+@register(outgoing=True, pattern='^A(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**PASANG PP DULU DEK,BIAR ORANG SEGRUP TAU BETAPA HINA NYA MUKA LU😆**")
@@ -109,5 +111,7 @@ CMD_HELP.update({
 \n\n`N`\
 \nUsage: Hujat Orang caper.\
 \n\n`J`\
-\nUsage: Hujat Jamet."
+\nUsage: Hujat Jamet.
+\n\n`A`\
+\nUsage: Hujat yang gapunya muka."
 })
