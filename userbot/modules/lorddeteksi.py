b92a7273304259188abfc7c50c3278b6a21cb5e0
Alvin <75377410+Zora24@users.noreply.github.com>
2021-03-28 04:00:10 +0700

    Update lorddeteksi.py

--- userbot/modules/lorddeteksi.py
+++ userbot/modules/lorddeteksi.py
@@ -3,7 +3,7 @@
 # Alvin Ganteng
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP
+from userbot import CMD_HELP, bot
 from userbot.events import register
 
 
