8fe31ad6143e95c81aea57b03e4ab9ab0e4d5c1d
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-03-28 17:42:05 +0700

    Create gcast.py

---
+++ userbot/modules/gcast.py
@@ -0,0 +1,36 @@
+# frm Ultroid
+# port by Koala @manusiarakitann
+# @LordUserbot_Group
+# Alvin Ganteng
+
+from userbot.events import register
+from userbot import CMD_HELP
+# Alvin Ganteng
+
+
+@register(outgoing=True, pattern="^.gcast (.*)")
+async def gcast(event):
+    xx = event.pattern_match.group(1)
+    if not xx:
+        return await event.edit("`Lord, Mohon Berikan Sebuah Pesan`")
+    tt = event.text
+    msg = tt[6:]
+    kk = await event.edit("`Sedang Mengirim Pesan Secara Global...`")
+    er = 0
+    done = 0
+    async for x in bot.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(f"`Berhasil Mengirim Pesan Ke {done} Obrolan, Gagal Mengirim Ke {er} Obrolan")
+
+# Alvin Ganteng
+CMD_HELP.update(
+    {
+        "gcast": ".gcast <pesan>\
+    \nPenjelasan: Global Broadcast mengirim pesan ke Seluruh Grup yang Lu Masukin."
+    })
