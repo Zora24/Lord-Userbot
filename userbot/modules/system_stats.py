56104aa499778f5b492a623a577e195f09f92217
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-03-29 23:42:12 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -262,6 +262,8 @@ async def amireallyalive(alive):
 async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
+    await alive.edit("**Perkenalan diri**")
+    await alive.edit("⭐")
     output = (
         f"**¥▬▬▬▬▬▬❙۩ᬉིུ֛ ⃝ ꦿꦶ ۩❙▬▬▬▬▬▬¥**\n"
         f"           **♕ STRESS USERBOT ♕** \n\n"
