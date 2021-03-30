5733b4d70e128b87538557cda392f883b7674893
Alvin <75377410+Zora24@users.noreply.github.com>
2021-03-31 03:03:37 +0700

    Update lordamong.py

--- userbot/modules/lordamong.py
+++ userbot/modules/lordamong.py
@@ -14,7 +14,7 @@ from time import sleep
 from userbot import ALIVE_NAME, CMD_HELP, bot
 from platform import uname
 from userbot.events import register
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "lord"
 
 
 @register(outgoing=True, pattern="imposter(|n) (.*)")
