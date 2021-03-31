9d74fc84433a840ff4ae63b056a20e2edd06ae88
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-04-01 01:32:43 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "㊗️ **Repo:** [STRESS-Userbot](https://github.com/ramadhani892/STRESS-Userbot)\n㊗️ **Pemilik:** [RAMA](t.me/Ramadhaniiiiiiiii1)\n㊗️ **CHANNEL:** [CANDU MUSIK](t.me/candu_musikk)\n㊗️ **GROUP:** [TEMAN RANDOM](t.me/teman_random)"
+        "㊗️ **Repo:** [STRESS-Userbot](https://github.com/ramadhani892/STRESS-Userbot)\n㊗️ **Pemilik:** [RAMA](t.me/Ramadhaniiiiiii1)\n㊗️ **CHANNEL:** [CANDU MUSIK](t.me/candu_musikk)\n㊗️ **GROUP:** [TEMAN RANDOM](t.me/teman_random)"
     )
 
 
