643d3cc718b129567496d4a4cb230e790f7ce31d
Alvin <75377410+Zora24@users.noreply.github.com>
2021-04-01 18:59:26 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Port From UniBorg to UserBot by MoveAngel
+
 
 import random
 import requests
