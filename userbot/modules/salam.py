9a1c28339cc9a54269a6c4ffd9736a783b29ab09
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-03 06:20:52 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,25 +10,25 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("⚡Assalamu'alaikum....kawan⚡")
+    await typew.edit("⚡`Assalamu'alaikum....kawan`⚡")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("⚡Assalamu'alaikum....kawan⚡")
+    await typew.edit("⚡`Assalamu'alaikum....kawan`⚡")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("🕊️Wa'alaikumussalam....🕊️")
+    await typew.edit("🕊️`Wa'alaikumussalam....`🕊️")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("🕊️Wa'alaikumussalam....🕊️")
+    await typew.edit("🕊️`Wa'alaikumussalam....`🕊️")
 
 
 CMD_HELP.update({
