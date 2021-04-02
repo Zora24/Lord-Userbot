85a61815f79af691ebe3fec1a9485deab2516ff9
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-03 06:19:19 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -90,7 +90,7 @@ async def type_afk_is_not_true(notafk):
     global afk_end
     user = await bot.get_me()  # pylint:disable=E0602
     last = user.last_name
-    if last and last.endswith("⚡【AFK】⚡"):
+    if last and last.endswith("⚡AFK⚡"):
         last1 = last[:-12]
     else:
         last1 = ""
@@ -98,7 +98,7 @@ async def type_afk_is_not_true(notafk):
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
         ISAFK = False
-        msg = await notafk.respond("**⚡Bos telah kenbali⚡!!**")
+        msg = await notafk.respond("**⚡Bos telah kembali⚡!!**")
         time.sleep(3)
         await msg.delete()
         await notafk.client(UpdateProfileRequest(first_name=user.first_name, last_name=last1))
