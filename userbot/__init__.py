e189f22029220bb5d7d2ecc8704313e0fab77740
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-05 13:29:21 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -319,7 +319,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_message(BOTLOG_CHATID, "```『Lord-Userbot Telah Aktif』```")
+    await bot.send_message(BOTLOG_CHATID, "```⚡GBX-bot Aktif Goblok⚡```")
     return
 
 with bot:
