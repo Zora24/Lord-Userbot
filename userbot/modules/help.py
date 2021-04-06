960523cd1c919b6a3d2e0dafc7f31d830f58c7b5
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-06 23:27:23 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -29,9 +29,9 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t⚡  "
-        await event.edit("**🌀 Daftar Perintah Untuk\nGBX-Userbot:\n\n**"
+        await event.edit("**⚠️ Daftar Perintah Untuk\nGBX-Userbot:\n\n**"
                          f"⚡{string}⚡"
-                         "\n🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
+                         "\n☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️")
         await event.reply(f"\n**Ketik Contoh** `.help afk` **Untuk Informasi Perintah**")
         await asyncio.sleep(1000)
         await event.delete()
