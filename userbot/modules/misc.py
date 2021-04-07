568ccefb8f854934f47891ea578235b76d7a2858
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-07 16:18:09 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "⚡ **Repo Userbot:** [GBX-Userbot](https://github.com/apisuserbot/GBX-Userbot)\n⚡ **Bos:** [Apis](t.me/PacarFerdilla)"
+        "**╭┄┅┯┅┄──────┄┅┯┅┄╮**\n┃\n**┃    ＼ ｜ ／ ʀᴇᴘᴏ ＼ ｜ ／**\n┃   [⚡GBX-Userbot⚡](https://github.com/apisuserbot/GBX-Userbot)\n**┃    ／ ｜ ＼           ／ ｜ ＼**\n┃\n**╰┄┅┷┅┄──────┄┅┷┅┄╯**\n"
     )
 
 
