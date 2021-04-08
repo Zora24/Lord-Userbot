9850330afae415457c465f693a05a86f6f1ee321
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-04-09 03:05:13 +0700

    Update Ramaganss.py

--- userbot/modules/Ramaganss.py
+++ userbot/modules/Ramaganss.py
@@ -355,7 +355,7 @@ async def typewriter(typew):
                      "            \\              (\n"
                      "              \\  ")
 
-@register(outgoing=True, pattern='^.tr(?: |$)(.*)')
+@register(outgoing=True, pattern='^.tp(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**INTINYA JANGAN LUPA MASUK!!**\n\n"
@@ -400,7 +400,7 @@ CMD_HELP.update({
     \nUsage: liat sendiri.\
     \n\n`.y` ; `.uasu`\
     \nUsage: jempol & Cek dyno\
-    \n\n`.tawa` ; `.oy` ; `.fck` ; `.tr`\
+    \n\n`.tawa` ; `.oy` ; `.fck` ; `.tp`\
     \nUsage: ketawa lari , Nyuruh nimbrung, fuck, Coba sendiri.\
     \n\n`.ular` ; `.babi` ; `.ajg`\
     \nUsage: liat sendiri."
