c497c903dd8932ae5f0d7d0a8f0d71d0d5b9221e
Alvin <75377410+Zora24@users.noreply.github.com>
2021-04-10 08:10:59 +0700

    Update kekuatan.py

--- userbot/modules/kekuatan.py
+++ userbot/modules/kekuatan.py
@@ -1,3 +1,38 @@
+
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+# Original source for the deepfrying code (used under the following
+# license): https://github.com/Ovyerus/deeppyer
+
+# MIT License
+#
+# Copyright (c) 2017 Ovyerus
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# Ported from Xtra-telegram by @heyworld
+
+# LORD USERBOT
+
 import os
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
@@ -131,6 +166,6 @@ async def _(fry):
 
 CMD_HELP.update({
     "kekuatan":
-    "`.kekuatan` or `.kekuatan` [level(1-8)]"
-    "\nUsage: untuk mengubah foto/sticker."
+    "**Modules: __Kekuatan__\n\n**Perintah:** `.kekuatan` / `.kekuatan [level(1-8)]`"
+    "\n**Penjelasan:** untuk mengubah foto/sticker."
 })
