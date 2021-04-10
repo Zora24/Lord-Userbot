ff1ab907f6fe141129c43b679959c871bc108717
Alvin <75377410+Zora24@users.noreply.github.com>
2021-04-10 07:32:57 +0700

    Update anti_spambot.py

--- userbot/modules/anti_spambot.py
+++ userbot/modules/anti_spambot.py
@@ -4,6 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
+# LORD USERBOT
 from asyncio import sleep
 
 from requests import get
@@ -19,12 +20,12 @@ from userbot import (
     bot,
 )
 
+# LORD USERBOT
 
 @bot.on(ChatAction)
 async def ANTI_SPAMBOTS(welcm):
+    """Cekal pengguna yang baru bergabung jika cocok dengan algoritma pemeriksaan spammer."""
     try:
-        """Ban a recently joined user if it
-        matches the spammer checking algorithm."""
         if not ANTI_SPAMBOT:
             return
         if welcm.user_joined or welcm.user_added:
@@ -80,12 +81,10 @@ async def ANTI_SPAMBOTS(welcm):
                     check_user = await welcm.client.get_entity(user_id)
 
                     # DEBUGGING. LEAVING IT HERE FOR SOME TIME ###
-                    print(
-                        f"User Joined: {check_user.first_name} [ID: {check_user.id}]")
+                    print(f"Pengguna Telah Bergabung: {check_user.first_name} [ID: {check_user.id}]")
                     print(f"Chat: {welcm.chat.title}")
-                    print(f"Time: {join_time}")
-                    print(
-                        f"Message Sent: {message.text}\n\n[Time: {message_date}]")
+                    print(f"Waktu: {join_time}")
+                    print(f"Pesan terkirim: {message.text}\n\n[Waktu: {message_date}]")
                     ##############################################
 
                     try:
@@ -95,12 +94,12 @@ async def ANTI_SPAMBOTS(welcm):
                         data = r.json()
                     except BaseException:
                         print(
-                            "CAS check failed, falling back to legacy anti_spambot behaviour."
+                            "Pemeriksaan CAS gagal, kembali ke legacy anti_spambot behaviour."
                         )
                         data = None
 
                     if data and data["ok"]:
-                        reason = f"[Banned by Combot Anti Spam](https://combot.org/cas/query?u={check_user.id})"
+                        reason = f"[Banned by Combot Anti Spam](https://cas.chat/query?u={check_user.id})"
                         spambot = True
                     elif "t.cn/" in message.text:
                         reason = "Match on `t.cn` URLs"
@@ -131,7 +130,7 @@ async def ANTI_SPAMBOTS(welcm):
                                 spambot = True
 
                     if spambot:
-                        print(f"Potential Spam Message: {message.text}")
+                        print(f"Potensi Pesan Spam: {message.text}")
                         await message.delete()
                         break
 
@@ -146,8 +145,8 @@ async def ANTI_SPAMBOTS(welcm):
                         await welcm.reply(
                             "@admins\n"
                             "`ANTI SPAMBOT DETECTOR!\n"
-                            "THIS USER MATCHES MY ALGORITHMS AS A SPAMBOT!`"
-                            f"REASON: {reason}"
+                            "PENGGUNA INI SESUAI DENGAN ALGORITMA SAYA SEBAGAI SPAMBOT!`"
+                            f"ALASAN: {reason}"
                         )
                         kicked = False
                         reported = True
@@ -155,10 +154,10 @@ async def ANTI_SPAMBOTS(welcm):
                     try:
 
                         await welcm.reply(
-                            "`Potential Spambot Detected !!`\n"
-                            f"`REASON:` {reason}\n"
-                            "Kicking away for now, will log the ID for further purposes.\n"
-                            f"`USER:` [{check_user.first_name}](tg://user?id={check_user.id})"
+                            "`Potensi Spambot Terdeteksi.!!`\n"
+                            f"`ALASAN   :` {reason}\n"
+                            "Mengeluarkan dia untuk saat ini.\n"
+                            f"`PENGGUNA :` [{check_user.first_name}](tg://user?id={check_user.id})"
                         )
 
                         await welcm.client.kick_participant(
@@ -172,8 +171,8 @@ async def ANTI_SPAMBOTS(welcm):
                             await welcm.reply(
                                 "@admins\n"
                                 "`ANTI SPAMBOT DETECTOR!\n"
-                                "THIS USER MATCHES MY ALGORITHMS AS A SPAMBOT!`"
-                                f"REASON: {reason}"
+                                "PENGGUNA INI SESUAI DENGAN ALGORITMA SAYA SEBAGAI SPAMBOT!`"
+                                f"ALASAN  : {reason}"
                             )
                             kicked = False
                             reported = True
@@ -183,21 +182,23 @@ async def ANTI_SPAMBOTS(welcm):
                         await welcm.client.send_message(
                             BOTLOG_CHATID,
                             "#ANTI_SPAMBOT REPORT\n"
-                            f"USER: [{users.first_name}](tg://user?id={check_user.id})\n"
-                            f"USER ID: `{check_user.id}`\n"
-                            f"CHAT: {welcm.chat.title}\n"
-                            f"CHAT ID: `{welcm.chat_id}`\n"
-                            f"REASON: {reason}\n"
-                            f"MESSAGE:\n\n{message.text}",
+                            f"Pengguna    : [{check_user.first_name}](tg://user?id={check_user.id})\n"
+                            f"ID Pengguna : `{check_user.id}`\n"
+                            f"Pengguna    : {welcm.chat.title}\n"
+                            f"ID Pesan    : `{welcm.chat_id}`\n"
+                            f"ALASAN      : {reason}\n"
+                            f"PESAN       :\n\n{message.text}",
                         )
     except ValueError:
         pass
 
+# LORD USERBOT
+# ALVIN GANTENG
 
 CMD_HELP.update(
     {
-        "anti_spambot": "If enabled in config.env or env var,"
-        "\nthis module will ban(or inform the admins of the group about) the"
-        "\nspammer(s) if they match the userbot's anti-spam algorithm."
+        "anti_spambot": "Jika diaktifkan di config.env atau env var,\
+        \nmodul ini akan melarang (atau memberi tahu admin grup tentang)\
+        \npelaku spam jika cocok dengan algoritm anti-spam bot pengguna."
     }
 )
