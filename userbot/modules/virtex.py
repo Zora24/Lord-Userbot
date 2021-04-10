7155a7bb34b663f1b95fa368d5abc66d7ce7e3cc
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-10 16:41:03 +0700

    Create virtex.py

---
+++ userbot/modules/virtex.py
@@ -0,0 +1,82 @@
+# Ported by @PacarFerdilla
+# Warning!!!
+# Credits by @TeamUltroid, Kraken, Sawan.
+
+from time import sleep
+from userbot import CMD_HELP, bot
+from userbot.events import register
+from random import getrandbits, randint
+from re import sub
+
+import requests
+import asyncio
+import random
+
+@register(outgoing=True, pattern='^.virtex?: |$)(.*)')
+async def virtex(event):
+    animation_interval = 0.7
+    animation_ttl = range(0, 11)
+    await event.edit("Installing.")
+    await event.edit("Installing..")
+    await event.edit("Installing...")
+    await asyncio.sleep(3)
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")   
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*") 
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*") 
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit(""*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*)
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")   
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*") 
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*") 
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit(""*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*)
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")   
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*") 
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*") 
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit(""*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*)
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await event.edit("*9999999* *ผิดุท้่เึางืผิดุท้่เึางื*")
+    await asyncio.sleep(3)
+    await event.edit("`...Termux...`")
+    await asyncio.sleep(3)
+    await event.edit("`virtex to : 62+********`)
+    await asyncio.sleep(3)
+    await event.edit("`Sukses Mengirim Pesan Virtex....`")
+    await asyncio.sleep(3)
+    await event.edit("`Syntax Err4r to approved handphone`")
+    await asyncio.sleep(3)
+    await event.edit("`Tunggulah HP Temanmu Akan Lag Seketika:)`")
+
+
+CMD_HELP.update({
+        "virtex": "Penjelasan: .virtex\
+        \n↳ : Untuk Mengirim Virtex Candaan Ke Temen via:v,Termux."
+})
