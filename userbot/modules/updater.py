fbc77636f9b9cec2aa9612acdf5b483ab49be6b4
Jerryadisaputraa <78988675+jerryadisaputra@users.noreply.github.com>
2021-04-14 07:33:02 +0000

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -67,7 +67,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             )
             return repo.__del__()
         await event.edit('`[HEROKU]:'
-                         '\nDyno STRESS-Userbot Sedang Dalam Proses, Mohon Menunggu 7-8 Menit`'
+                         '\nDyno GEMBEL-ELITE Userbot Sedang Dalam Proses, Mohon Menunggu 7-8 Menit`'
                          )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
