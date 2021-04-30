3cc347beb7231d14157a06e895945dcd13b53c2b
istammuyee <83205292+istammuyee@users.noreply.github.com>
2021-04-30 18:32:02 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -2,15 +2,15 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.alvin(?: |$)(.*)')
+@register(outgoing=True, pattern='^.amm(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
-    await typew.edit("`Hai Perkenalkan Namaku Alvin`")
+    await typew.edit("𝙃𝙖𝙞 𝙋𝙚𝙧𝙠𝙚𝙣𝙖𝙡𝙠𝙖𝙣 𝙉𝙖𝙢𝙖𝙠𝙪 𝘼𝙢𝙢")
     sleep(3)
-    await typew.edit("`17 Tahun`")
+    await typew.edit("19 𝙏𝙖𝙝𝙪𝙣")
     sleep(1)
-    await typew.edit("`Tinggal Dibatam, Salam Kenal:)`")
+    await typew.edit("𝙏𝙞𝙣𝙜𝙜𝙖𝙡 𝘿𝙞 𝙅𝙖𝙬𝙖,𝘼𝙨𝙡𝙞 𝙈𝙖𝙡𝙖𝙮𝙨𝙞𝙖 𝙎𝙖𝙡𝙖𝙢 𝙆𝙚𝙣𝙖𝙡:)")
 # Create by myself @localheart
 
 
@@ -20,19 +20,19 @@ async def typewriter(typew):
     sleep(3)
     await typew.edit("`Cuma Mau Bilang`")
     sleep(3)
-    await typew.edit("`Aku Sayang Kamu`")
+    await typew.edit("`Aku Sayang Mawar`")
     sleep(1)
     await typew.edit("`I LOVE YOU 💞`")
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
+@register(outgoing=True, pattern='^.pantek(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(3)
     await typew.edit("`Apapun Yang Terjadi`")
     sleep(3)
-    await typew.edit("`Tetaplah Bernapas`")
+    await typew.edit("`Jangalah panik`")
     sleep(1)
-    await typew.edit("`Dan Selalu Bersyukur`")
+    await typew.edit("`Dan Selalu Katakan 𝘽𝙔 𝙊𝙉𝙀 𝙋𝙖𝙣𝙩𝙚𝙠`")
 # Create by myself @localheart
