fb361c95f5be67dcc7f19ade3ecf297db7e79471
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-05-11 02:29:20 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -34,7 +34,7 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/Zora24/Lord-Userbot.git
+git clone https://github.com/jokokendi/Lord-Userbot/blob/Lord-Userbot.git
 cd Lord-Userbot
 
 pip install --upgrade pip setuptools
