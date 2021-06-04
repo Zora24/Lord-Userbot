ce425b904b15a365000cfc750a222e7f81bffa7f
azel-kochoyy <85310436+azel-kochoyy@users.noreply.github.com>
2021-06-04 14:46:03 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -2,8 +2,8 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
-# inline credit @keselekpermen69
-# Pengguna Lord-Userbot
+# inline credit @diabloyy
+# Pengguna diablo-Userbot
 """ Userbot initialization. """
 
 import os
